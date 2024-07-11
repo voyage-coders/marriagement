48ec0e3d94d50df9d5cc29ef83cdea6b4bd9005a
ShaRaf <sharafanees@gmail.com>
2024-07-11 16:59:13 +0500

    added image links
    added image links

--- client/src/components/services-section/servicesData.js
+++ client/src/components/services-section/servicesData.js
@@ -1,16 +1,46 @@
-import sampleImage from '../../assets/images/services-section/sample.png';
+import wear from '../../assets/images/services-section/1-bride-groom-wear.png';
+import salon from '../../assets/images/services-section/2-salon-and-spa.png';
+import jewelry from '../../assets/images/services-section/3-bridal-jewelry-accessories.png';
+import venue from '../../assets/images/services-section/4-venue.png';
+import photographer from '../../assets/images/services-section/5-photographer-videographer.png';
+import florist from '../../assets/images/services-section/6-decorations-florist.png';
+import catering from '../../assets/images/services-section/7-catering-service.png';
+import bakery from '../../assets/images/services-section/8-cake-bakery.png';
+import music from '../../assets/images/services-section/9-music-dj.png';
+import planner from '../../assets/images/services-section/10-wedding-event-planner.png';
+import officiants from '../../assets/images/services-section/11-officiants.png';
+import gifts from '../../assets/images/services-section/12-wedding-gifts.png';
+import travel from '../../assets/images/services-section/13-cars-travel.png';
+import favours from '../../assets/images/services-section/14-wedding-favours.png';
+import booth from '../../assets/images/services-section/15-photo-booth.png';
+import honeymoon from '../../assets/images/services-section/16-honeymoon.png';
+import guestbook from '../../assets/images/services-section/17-album-guestbook.png';
+import fireworks from '../../assets/images/services-section/18-fireworks.png';
+import dance from '../../assets/images/services-section/19-dance-choreographer.png';
+import spa from '../../assets/images/services-section/20-spa.png';
+
 
 const servicesData = [
-    { image: sampleImage, text: 'Card 1' },
-    { image: sampleImage, text: 'Card 2' },
-    { image: sampleImage, text: 'Card 3' },
-    { image: sampleImage, text: 'Card 4' },
-    { image: sampleImage, text: 'Card 5' },
-    { image: sampleImage, text: 'Card 6' },
-    { image: sampleImage, text: 'Card 7' },
-    { image: sampleImage, text: 'Card 8' },
-    { image: sampleImage, text: 'Card 9' },
-    { image: sampleImage, text: 'Card 10'},
+    { image: wear, text: 'Bridal & Groom Wear' },
+    { image: salon, text: 'Salon & Spa' },
+    { image: jewelry, text: 'Bridal Jewelry' },
+    { image: venue, text: 'Venue' },
+    { image: photographer, text: 'Photographer & Videographer' },
+    { image: florist, text: 'Florist' },
+    { image: catering, text: 'Catering Service' },
+    { image: bakery, text: 'Cake & Bakery' },
+    { image: music, text: 'Music & Dj' },
+    { image: planner, text: 'Wedding Planner'},
+    { image: officiants, text: 'Officiants'},
+    { image: gifts, text: 'Wedding Gifts'},
+    { image: travel, text: 'Car & Travel'},
+    { image: favours, text: 'Wedding Favours'},
+    { image: booth, text: 'Photo Booth'},
+    { image: honeymoon, text: 'Honeymoon'},
+    { image: guestbook, text: 'Album & Guestbook'},
+    { image: fireworks, text: 'Fireworks'},
+    { image: dance, text: 'Dance Choreographer'},
+    { image: spa, text: 'Luxury Spa'},
   ];
 
   export default servicesData
