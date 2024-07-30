079e60bf5ca4a58951cc980769a41aa43a018072
ShaRaf <60271901+sharafcs50@users.noreply.github.com>
2024-07-30 04:21:50 -0700

    Update MenuItems.jsx

--- client/src/components/header/MenuItems.jsx
+++ client/src/components/header/MenuItems.jsx
@@ -1,53 +1,90 @@
 export const MenuItems = [
-
-        {
-          category: 'Pre Event',
-          items: [
-            { title: 'SEO', path: '/marketing/seo', cName: 'dropdown-link' },
-            { title: 'Content Marketing', path: '/marketing/content', cName: 'dropdown-link' },
-          ],
-        },
-        {
-          category: 'During Event',
-          items: [
-            { title: 'Graphic Design', path: '/design/graphic', cName: 'dropdown-link' },
-            { title: 'UI/UX Design', path: '/design/uiux', cName: 'dropdown-link' },
-          ],
-        },
-        {
-          category: 'After Event',
-          items: [
-            { title: 'Business Consulting', path: '/consulting/business', cName: 'dropdown-link' },
-            { title: 'Financial Consulting', path: '/consulting/financial', cName: 'dropdown-link' },
-          ],
-        },
-        {
-          category: 'Other',
-          items: [
-            { title: 'B2B Sales', path: '/sales/b2b', cName: 'dropdown-link' },
-            { title: 'B2C Sales', path: '/sales/b2c', cName: 'dropdown-link' },
-          ],
-        },
-      ];
-
-
-    // {
-    //     title: 'Marketing',
-    //     path: '/marketing',
-    //     cName: 'dropdown-link'
-    // },
-    // {
-    //     title: 'Design',
-    //     path: '/design',
-    //     cName: 'dropdown-link'
-    // },
-    // {
-    //     title: 'Consulting',
-    //     path: '/consulting',
-    //     cName: 'dropdown-link'
-    // },
-    // {
-    //     title: 'Sales',
-    //     path: '/services',
-    //     cName: 'dropdown-link'
-    // }
+	{
+		category: "Pre Event",
+		items: [
+			{ title: "Venue", path: "/service/venue", cName: "dropdown-link" },
+			{
+				title: "Wedding Planner",
+				path: "/service/wedding-planner",
+				cName: "dropdown-link",
+			},
+			{
+				title: "Beauty Salon",
+				path: "/service/salon",
+				cName: "dropdown-link",
+			},
+			{
+				title: "Catering Service",
+				path: "/service/catering",
+				cName: "dropdown-link",
+			},
+			{
+				title: "Bridal Jewelry",
+				path: "/service/jewelry",
+				cName: "dropdown-link",
+			},
+			{
+				title: "Bridal & Groom Wear",
+				path: "/service/bridal-groom-wear",
+				cName: "dropdown-link",
+			},
+			{
+				title: "Officiants",
+				path: "/service/officiants",
+				cName: "dropdown-link",
+			},
+		],
+	},
+	{
+		category: "During Event",
+		items: [
+			{
+				title: "Photographer & Videographer",
+				path: "/service/photo-video",
+				cName: "dropdown-link",
+			},
+			{ title: "Cake & Bakery", path: "/service/bakery", cName: "dropdown-link" },
+			{ title: "Florist", path: "/service/florist", cName: "dropdown-link" },
+			{
+				title: "Music, DJ & Entertainment",
+				path: "/service/music-dj-entertainment",
+				cName: "dropdown-link",
+			},
+			{ title: "Car & Travel", path: "/service/car-travel", cName: "dropdown-link" },
+			{ title: "Photo Booth", path: "/service/photo-booth", cName: "dropdown-link" },
+		],
+	},
+	{
+		category: "After Event",
+		items: [
+			{
+				title: "Honeymoon",
+				path: "/service/honeymoon",
+				cName: "dropdown-link",
+			},
+			{
+				title: "Album & Guestbook",
+				path: "/service/album-guestbook",
+				cName: "dropdown-link",
+			},
+		],
+	},
+	{
+		category: "Other",
+		items: [
+			{ title: "Wedding Favours", path: "/service/wedding-favours", cName: "dropdown-link" },
+			{
+				title: "Dance Choreographer",
+				path: "/service/dance-choreographer",
+				cName: "dropdown-link",
+			},
+			{ title: "Fireworks", path: "/service/fireworks", cName: "dropdown-link" },
+			{ title: "Spa", path: "/service/spa", cName: "dropdown-link" },
+			{
+				title: "Wedding Gifts",
+				path: "/service/wedding-gifts",
+				cName: "dropdown-link",
+			},
+		],
+	},
+];
